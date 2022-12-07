fa335b516d1ecea5dea154c9803c403fa4944dbe
Gerardo Miranda <gerardodev25@gmail.com>
2022-12-07 13:39:02 -0400

    Generar JWT en el servicio

--- routes/auth.router.js
+++ routes/auth.router.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const passport = require('passport');
 
+const jwt = require('jsonwebtoken');
+const { config } = require('../congif/config');
+
 const router = express.Router();
 
 router.post(
@@ -8,7 +11,14 @@ router.post(
   passport.authenticate('local', { session: false }),
   async (req, res, next) => {
     try {
-      res.json(req.user);
+      const { user } = req;
+
+      const payload = {
+        sub: user.id,
+        role: user.role,
+      };
+      const token = jwt.sign(payload, config.jwtSecret);
+      res.json({ user, token });
     } catch (error) {
       next(error);
     }
